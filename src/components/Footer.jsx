12fc1118800bdaa00fc14b4dc4dd7ac9fe3ca813
Todd Albert <todd@bocacode.com>
2022-10-27 16:16:06 -0400

    horror movie site

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2022 Todd Albert. 
+        Source: <a href="https://sampleapis.com/api-list/movies">
+          sampleapis.com</a> </p>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/components/MovieList.jsx
@@ -0,0 +1,20 @@
+import movies from '../movies'
+
+function MovieList() {
+  return (
+    <section id="movie-list-container">
+      {movies.map(movie => (
+        <div className='movie-card' key={movie.id}>
+          <img className='movie-poster'
+            src={movie.posterURL}
+            alt={movie.title} />
+          <h3 className='movie-title'>
+            {movie.title}
+          </h3>
+        </div>
+      ))}
+    </section>
+  )
+}
+
+export default MovieList;
