f3d8af3ee2948dc393cef3f67e512c663f1f89c4
Midokhalil1 <midokhalil22@gmail.com>
2022-11-09 11:04:38 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import Header from './components/Header';
-import MovieList from './components/MovieList';
-import Footer from './components/Footer';
-import './App.css';
+import Header from "./components/Header";
+import MovieList from "./components/MovieList";
+import Footer from "./components/Footer";
+import "./App.css";
+import MovieComponent from "./components/MovieComponent";
 
 function App() {
+  const movieInfo = {
+    Id: "1",
+    posterURL:
+      "https://bocacode.com/assets/images/bc-logos/boca-code-rgb-logo-tag-reversed.png",
+    title: "Boca Code the Movie ",
+  };
   return (
     <div className="App">
       <Header />
+      <MovieComponent movieProp={movieInfo} />
       <MovieList />
       {/* we will change this  */}
       {/* eventually*/}
---
+++ src/components/MovieComponent.jsx
@@ -0,0 +1,25 @@
+function MovieComponent ({movieProp}) {
+    return (
+
+
+<div className='movie-card' key={movieProp.id}>
+          <img className='movie-poster'
+            src={movieProp.posterURL}
+            alt={movieProp.title} />
+          <h3 className='movie-title'>
+            {movieProp.title}
+          </h3>
+        </div>
+
+
+
+    )
+
+
+
+
+
+
+}
+
+export default MovieComponent
